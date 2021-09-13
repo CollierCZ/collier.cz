e7a5fcc02c7a0add63a58dbf96848bf857140e32
Aaron Collier <aaron@collier.cz>
2021-09-13 13:01:10 +0200

    chore: cleanup

--- aleph/lib/useStrapi.ts
+++ aleph/lib/useStrapi.ts
@@ -1,13 +1,13 @@
 import { useDeno } from "https://deno.land/x/aleph/framework/react/mod.ts"
 
 const useStrapi = (endpoint: String) => {
-    return useDeno(async () => {
-        const relationships = JSON.parse(atob(Deno.env.get('PLATFORM_RELATIONSHIPS')))
-        const strapi = relationships.strapi[0]
-        const STRAPI_API_URL = `http://${strapi.ip}:${strapi.port}` || "http://localhost:1337";
-        const url = `${STRAPI_API_URL}/${endpoint}`;
-            return await (await fetch(url)
-            .then(response => response.json()))
-  })}
+  return useDeno(async () => {
+    const relationships = JSON.parse(atob(Deno.env.get('PLATFORM_RELATIONSHIPS')))
+    const strapi = relationships.strapi[0]
+    const STRAPI_API_URL = `http://${strapi.ip}:${strapi.port}`;
+    const url = `${STRAPI_API_URL}/${endpoint}`;
+    return await (await fetch(url).then(response => response.json()))
+  })
+}
 
 export default useStrapi
