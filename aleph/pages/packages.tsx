2271136012eafd25e82a396a403bd72fe81fa3f2
Aaron Collier <aaron@collier.cz>
2021-10-12 15:15:31 +0200

    fix: styling

--- aleph/pages/packages.tsx
+++ aleph/pages/packages.tsx
@@ -10,12 +10,12 @@ interface Package {
   const Packages = () => {
     const packages = useStrapi("packages")
     return (
-        <div className="w-screen h-screen flex items-center justify-center">
-          <h1 className="text-4x1 font-extrabold leading-relaxed">Hello, World!</h1>
-          <table>
+        <div className="w-screen h-screen flex flex-col items-center justify-center">
+          <h1 className="text-4xl font-extrabold leading-relaxed">Hello, World!</h1>
+          <table className="border border-gray-500">
             <thead>
               <tr>
-                <th scope="col" className="bg-gray-400">Name</th>
+                <th scope="col" className="bg-gray-400 p-8">Name</th>
                 <th scope="col" className="bg-gray-400">Description</th>
                 <th scope="col" className="bg-gray-400">Stars</th>
                 <th scope="col" className="bg-gray-400">URL</th>
@@ -24,11 +24,11 @@ interface Package {
             <tbody>
               {packages.map((pkg: Package) => (
                 <tr key={pkg.name}>
-                  <th scope="row" className="bg-gray-400">{pkg.name}</th>
-                  <td>{pkg.description}</td>
-                  <td>{pkg.stars}</td>
-                  <td>
-                    <a href={pkg.github}>{pkg.github}</a>
+                  <th scope="row" className="bg-gray-400 p-8">{pkg.name}</th>
+                  <td className="p-8">{pkg.description}</td>
+                  <td className="p-8">{pkg.stars}</td>
+                  <td className="p-8">
+                    <a href={pkg.github} className="underline hover:no-underline focus:no-underline">{pkg.github}</a>
                   </td>
                 </tr>
               ))}
