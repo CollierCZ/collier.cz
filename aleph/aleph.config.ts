a8c02f3c94389a5f73c5c46e3e3f2e159e7cb0cc
Aaron Collier <aaron@collier.cz>
2022-01-06 13:31:50 +0100

    :fire: Remove Aleph and Strapi

--- aleph/aleph.config.ts
+++
@@ -1,9 +0,0 @@
-import windicss from 'https://deno.land/x/aleph_plugin_windicss@v0.0.2/plugin.ts'
-import type { Config } from 'aleph/types'
-import mdx from "./plugins/mdx.ts"
-
-export default (): Config => ({
-  plugins: [
-    mdx(),windicss
-  ]
-})
