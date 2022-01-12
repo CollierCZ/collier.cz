1e8bf6f49c5bc32ec51c1b839ea71028de2b01f0
Aaron Collier <aaron@collier.cz>
2022-01-12 23:26:15 +0100

    :lipstick: Adjust tag borders

--- svelte/src/components/ArticleTags.svelte
+++ svelte/src/components/ArticleTags.svelte
@@ -3,10 +3,10 @@
 </script>
 
 {#if tags}
-  <div class="flex flex-wrap text-green-700 inline text-base">
+  <div class="flex flex-wrap text-green-700 inline text-sm lg:text-base">
     {#each tags as tag}
-      <div class="border-2 mr-4 mt-2 rounded-md border-green-700 hover:border-fuchsia-700 bg-green-50 hover:bg-fuchsia-50 py-1">
-        <a sveltekit:prefetch class="px-2 py-1" href="/tag/{tag}">{tag}</a>
+      <div class="border-1 mr-1 mt-2 rounded-md border-green-300 hover:border-fuchsia-300 bg-green-50 hover:bg-fuchsia-50 py-0.5">
+        <a sveltekit:prefetch class="px-2 py-2" href="/tag/{tag}">{tag}</a>
       </div>
     {/each}
   </div>
