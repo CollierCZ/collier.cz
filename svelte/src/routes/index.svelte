83b11fcda93f21c790361b8d45cef6451c2c9a56
Aaron Collier <aaron@collier.cz>
2022-01-12 10:24:07 +0100

    :sparkles: Add media queries
    and fix layout shift on listing page

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -23,18 +23,34 @@
 <script lang="ts">
   import ArticleCard from "../components/ArticleCard.svelte"
 	import { sortArticles } from '$lib/utilities'
+  import Avatar from "../images/avatar.png?w=300;400;800&format=webp&srcset"
+  import AvatarPlaceholder from "../images/avatar.png?w=10"	
+	import { media } from "$lib/mediaQueries"
 
 	export let articles: Array<ArticleMetadata>;
 	articles = sortArticles(articles)
 </script>
 
-<aside class="text-center items-center justify-center my-8 sm:flex sm:text-left">
+<aside class="text-center items-center justify-center max-w-xs sm:max-w-80ch mx-auto sm:mx-0 my-8 sm:flex sm:text-left">
 	<div class="sm:pl-8 prose lg:prose-lg sm:order-2">
 		<h1 class="text-3xl font-extrabold leading-relaxed">Aaron Collier</h1>
 		<p>Originally from Lexington and now residing in Brno, Aaron is an educator, editor, and elucidator. <a href="https://collier.cz/aaron-collier" class="underline hover:no-underline focus:no-underline">More about Aaron</a>.</p>
 	</div>
-	<div class="sm:order-1 w-48 sm:w-auto mx-auto">
-		<img alt="" src="/avatar.png" />
+	<div class="sm:order-1 w-auto sm:w-32 sm:flex-shrink-0 lg:w-36 mx-auto">
+		<picture>
+			<source
+				srcset={Avatar}
+				sizes="(max-width: 800px) 100vw, 800px"
+			/>
+			<img
+				class="p-0"
+				src={AvatarPlaceholder}
+				alt=""
+				style="background-size: cover;background-image: url({AvatarPlaceholder});"
+				width={$media.large ? "144" : $media.small ? "128" : "320"}
+				height={$media.large ? "144" : $media.small ? "128" : "320"}
+			/>
+		</picture>
 	</div>
 </aside>
 
