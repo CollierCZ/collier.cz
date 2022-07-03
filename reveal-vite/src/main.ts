758b128a915f47bbf69dcdc7486b62fabaf6c53f
Aaron Collier <aaron@collier.cz>
2022-07-03 21:22:47 +0200

    ✨ Add speaker view

--- reveal-vite/src/main.ts
+++ reveal-vite/src/main.ts
@@ -3,9 +3,10 @@ import 'reveal.js/dist/theme/solarized.css'
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown';
 import Highlight from 'reveal.js/plugin/highlight/highlight';
+import Notes from 'reveal.js/plugin/notes/notes'
 
 const deck = new Reveal({
-  plugins: [ Highlight, Markdown ]
+  plugins: [ Highlight, Markdown, Notes ]
 })
 deck.initialize({
   hash: true,
