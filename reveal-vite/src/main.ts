93088c2be754f2fc02cd70e918051b6fd41838b4
Aaron Collier <aaron@collier.cz>
2022-07-03 17:51:27 +0200

    ✨ Add highlighting

--- reveal-vite/src/main.ts
+++ reveal-vite/src/main.ts
@@ -2,13 +2,14 @@ import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/solarized.css'
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown';
+import Highlight from 'reveal.js/plugin/highlight/highlight';
 
 const deck = new Reveal({
-  plugins: [ Markdown ]
+  plugins: [ Highlight, Markdown ]
 })
 deck.initialize({
   hash: true,
-  slideNumber: true,
+  slideNumber: false,
   markdown: {
     smartypants: true
   },
